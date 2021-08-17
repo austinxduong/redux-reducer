3e44948bd8997f6ee6eb2a148a9dacdf836b495a
austinxduong <axduong2@gmail.com>
2021-08-17 11:59:34 -0700

    wrapped ReduxProvider in app.js

---
+++ src/components/state/actions.js
@@ -0,0 +1,16 @@
+export const CURRENT_COLOR = 'CURRENT_COLOR';
+export const UNDO = 'UNDO';
+export const REDO = 'REDO';
+
+export const currentColor = (current) => ({
+  type: CURRENT_COLOR,
+  payload: current,
+});
+
+export const undoColor = () => ({
+  type: UNDO,
+});
+
+export const redoColor = () => ({
+  type: REDO,
+});
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
+import { ReduxProvider } from './components/state/ReduxProvider';
+import { initialState, Reducer } from './state/reducer.js';
 
 render(
-  <App />,
+  <ReduxProvider reducer={Reducer} initialState={initialState}>
+    <App />
+  </ReduxProvider>,
   document.getElementById('root')
 );
