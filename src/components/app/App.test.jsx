2247658cc05babae84517ddcb46ebff34bda2892
austinxduong <axduong2@gmail.com>
2021-08-18 11:50:44 -0700

    first behavior test passes!

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,22 @@
+/* eslint-disable max-len */
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { ReduxProvider } from '../state/ReduxProvider';
+import { Reducer, initialState } from '../state/reducer';
 
-describe('App component', () => {
-  afterEach(() => cleanup());
-  it('renders App', () => {
-    const { asFragment } = render(<App />);
-    expect(asFragment()).toMatchSnapshot();
+
+describe('behavior testing!', () => {
+  it('renders presentational compononet color box', async () => {
+    render(<ReduxProvider reducer={Reducer} initialState={initialState}><App /></ReduxProvider>);
+
+    const display = screen.getByTestId('display');
+
+    const colorInput = screen.getByTestId('colorInput');
+    fireEvent.change(colorInput, { target: { value: '#40C69D' } });
+    expect(display).toHaveStyle({ backgroundColor: '#40C69D' });
+
+    // const { asFragment } = render(<App />);
+    // expect(asFragment()).toMatchSnapshot();
   });
 });
