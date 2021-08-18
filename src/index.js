88b25b70f9085dab3d03d14f0a9840943683b36a
austinxduong <axduong2@gmail.com>
2021-08-18 10:33:18 -0700

    fixed some bugs

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/app/App';
 import { ReduxProvider } from './components/state/ReduxProvider';
-import { initialState, Reducer } from './state/reducer.js';
+import { initialState, Reducer } from './components/state/reducer';
 
 render(
   <ReduxProvider reducer={Reducer} initialState={initialState}>
